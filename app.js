ee2f6c87de9ea0db696a26735c13f10aef7b2b47
Botoko09 <ngangasteven78@gmail.com>
2026-09-18 02:27:01 +0100

    Ajout du frontend auteurs et configuration Express

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const auteurRoutes = require('./routes/auteurRoutes');
+const membreRoutes = require('./routes/membreRoutes');
+const livreRoutes = require('./routes/livreRoutes');
+const empruntRoutes = require('./routes/empruntRoutes');
+const statistiqueRoutes = require('./routes/statistiqueRoutes');
+const loggerMiddleware = require('./middlewares/loggerMiddleware');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(loggerMiddleware);
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, World');
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-app.listen(3000);
+app.use('/api/auteurs', auteurRoutes);
+app.use('/api/membres', membreRoutes);
+app.use('/api/livres', livreRoutes);
+app.use('/api/emprunts', empruntRoutes);
+app.use('/api/statistiques', statistiqueRoutes);
+
+
+app.use(errorMiddleware);
+
+module.exports = app;
